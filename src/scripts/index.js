f00adbf1f5dd29aff755692f2e321e40da0f6573
Aria Bagaswara <ariabagaswara71@gmail.com>
2022-12-26 12:47:07 +0700

    Add Restaurant List, improving mobile responsive

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,7 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 import '../styles/responsive.css';
+import data from '../DATA.json';
 
 const hamburger = document.querySelector('#hamburger');
 const hero = document.querySelector('.hero');
@@ -20,4 +21,27 @@ main.addEventListener('click', function () {
     drawer.classList.remove('open');
 });
 
-console.log('Hello Coders! :)');
+data.restaurants.forEach((restaurant) => {
+    const dataset = `
+       <article class="restaurant-item">
+            <div class="image_wrapper">
+                <img class="restaurant-image" src="${restaurant.pictureId}" alt="Restaurant Image">
+                <div class="overlay rating">
+                    <p class="restaurant-rating"><span class="material-symbols-outlined">star</span> ${restaurant.rating}</p>
+                </div>
+            </div>
+            <div class="restaurant-content">
+            <p class="restaurant-location"><span class="material-symbols-outlined">
+            location_on
+            </span> ${restaurant.city}</p>
+            <h1 class="restaurant-name">${restaurant.name}</h1>
+            <p class="restaurant-description">${restaurant.description.substr(0, 150)}</p>
+            <div class="restaurant-footer">
+                <a class="restaurant-button" href="#">Lihat Detail</a>
+            </div>
+            </div>
+       </article> 
+    `;
+
+    document.getElementById("restaurant-list").innerHTML += dataset;
+})
