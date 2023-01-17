6736ff48be388df568117a44171f4f2f78d32b58
Aria Bagaswara <ariabagaswara71@gmail.com>
2023-01-17 07:02:50 +0700

    disable analyzer mode

--- webpack.prod.js
+++ webpack.prod.js
@@ -20,6 +20,11 @@ module.exports = merge(common, {
       },
     ],
   },
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'disabled',
+    }),
+  ],
   optimization: {
     splitChunks: {
       chunks: 'all',
